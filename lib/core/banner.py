140123e154be42af4bd83dc8aa789978a2c2d782
cs <66363384+ecriminal@users.noreply.github.com>
2020-12-04 15:56:01 +0100

    Update banner.py

--- lib/core/banner.py
+++ lib/core/banner.py
@@ -7,5 +7,5 @@ BANNER = f'''{Fore.LIGHTBLUE_EX}       _                  _
  | '_ \\ ' \\| '_ \\ V / || | | ' \\ 
  | .__/_||_| .__/\\_/ \\_,_|_|_||_| {Fore.RESET}v{__version__}{Fore.LIGHTBLUE_EX}
  |_|       |_|
- {Fore.RESET}by checksum (@computergoon)
+ {Fore.RESET}by checksum (@0xFADE)
 '''
